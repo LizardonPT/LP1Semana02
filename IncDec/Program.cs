289e5de72644d4fb61e86287ac21fababf69e5e2
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-03 14:07:14 +0000

    Sufixo

---
+++ IncDec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = -7;
+
+            Console.WriteLine(a++ - b++);
+        }
+    }
+}
