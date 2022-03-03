2127bed274cd86648786836b75ad7a3ff51a783b
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-03 14:37:59 +0000

    Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduz a altura: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nIntroduz o raio: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double pi = 3.1415926;
+            double area = 2*pi*r*(r+a);
+            double volume = pi*Math.Pow(r, 2)*a;
+            
+            Console.WriteLine($"\nA área da superficie é de {area}");
+            Console.WriteLine($"O volume é de {volume}");
+        }
+    }
+}
