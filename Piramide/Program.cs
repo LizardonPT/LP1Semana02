55cbac0953b33ef247d2447e0033627e63fac37c
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-07 13:44:55 +0000

    Piramide

---
+++ Piramide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduza o número de linhas: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int space = 1; space <= (n-i); space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int a = 1; a <= i; a++)
+                {
+                    Console.Write("*");
+                }
+                for (int a = (i-1); a >= 1; a--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
